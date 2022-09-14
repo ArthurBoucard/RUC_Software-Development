bed8aae0d3e94764b9a07d8b9c8839215b15b198
arthurboucard <arthur.boucard@hotmail.fr>
2022-09-14 10:28:54 +0200

    rm useless import

--- src/com/portfolio1/UnitTests.java
+++ src/com/portfolio1/UnitTests.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
--- src/com/portfolio1/boat.java
+++ src/com/portfolio1/boat.java
@@ -1,9 +1,7 @@
 package com.portfolio1;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Vector;
 
 public class boat {
     public String flag;
